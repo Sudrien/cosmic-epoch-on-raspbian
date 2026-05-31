a91b4250a94be96ccd1fca2fa4b99fb1a2415541
Sudrien <Sudrien@users.noreply.github.com>
2026-05-31 19:43:08 -0400

    git submodule foreach git lfs pull --all

--- build-cosmic-sysext.sh
+++ build-cosmic-sysext.sh
@@ -515,6 +515,7 @@ if [[ "$INSTALL_ONLY" -eq 0 ]]; then
   git checkout --force "$REF"
   log "Syncing submodules (27 components -- this pulls a lot)..."
   git submodule update --init --recursive --force
+  git submodule foreach git lfs pull --all
 
   # -------------------------------------------------------------------------
   # 5. Build the system extension tree
